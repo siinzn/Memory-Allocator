f8fa0cfec4be21381ba2dd39e138086420323d5b
Sinan <msinannoufal@gmail.com>
2025-11-26 22:27:01 +0400

    Add project files.

---
+++ buffer.h
@@ -0,0 +1,24 @@
+
+class Buffer {
+private:
+    size_t MemorySize;
+    char* dynamicArray;
+public:
+    Buffer(size_t s) : MemorySize(s) {
+        dynamicArray = new char[s];
+    }
+
+    size_t get_size_of_memory() {
+        return MemorySize;
+    }
+
+    void* get_start_address() {
+        return static_cast<void*>(dynamicArray);
+    }
+
+    ~Buffer() {
+        delete[] dynamicArray;
+        dynamicArray = nullptr;
+    }
+};
+
